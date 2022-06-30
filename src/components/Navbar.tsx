a3f52042cef0e25623baa5b130452dfcc28cc665
Àlex Grabulosa Puertas <agrabulosa@re7systems.com>
2022-06-30 11:07:57 +0200

    Feat: Navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export function Navbar() {
     return(
-        <nav style={{ textAlign: 'center', padding: '1rem' }}>
+        <nav style={styles.navbar}>
             <ul style={styles.menu}>
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
@@ -10,6 +10,14 @@ export function Navbar() {
 }
 
 const styles = {
+    navbar: {
+        background: '#676767',
+        padding: '1rem',
+        top: '0',
+        zIndex: 1,
+        position: 'sticky' as 'sticky',
+        textAlign: 'center' as 'center',
+    },
     menu: {
         width: '50vw',
         display: 'inline-flex',
