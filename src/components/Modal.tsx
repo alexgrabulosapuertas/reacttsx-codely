09d33320d45702049b282a73ae3d2703f7349ea9
Àlex Grabulosa Puertas <agrabulosa@re7systems.com>
2022-06-30 11:22:21 +0200

    Feat: Modal Styles into const styles

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,10 +3,10 @@ import { ReactElement } from "react";
 export function Modal(props: { title: string, close: () => void, children: ReactElement }) {
 
     return (
-        <article style={{...styles.modal,  position: 'fixed' }}>
+        <article style={styles.modal}>
             <div style={{margin: '2rem'}}>
                 <h3>{ props.title }</h3>
-                <button style={{ position: 'absolute', top: 0, right: 0}} onClick={props.close}>Close</button>
+                <button style={styles.closeButton} onClick={props.close}>Close</button>
                 {props.children}
             </div>
         </article>
@@ -21,7 +21,13 @@ const styles = {
         width: '80vw',
         top: '50vh',
         left: '50vw',
+        position: 'fixed' as 'fixed',
         transform: 'translate(-50%, -50%)',
         boxShadow: '0px 0px 5px 2px',
+    },
+    closeButton: {
+        position: 'absolute' as 'absolute',
+        top: 0,
+        right: 0,
     },
 };
